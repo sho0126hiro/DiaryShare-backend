34d9e4e609af78931f6f7b639ebc8432b322e6b5
sho0126hiro <sho0126hiro@gmail.com>
2020-07-20 01:10:05 +0900

    ok

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -8,7 +8,7 @@ Rails.application.configure do
 
   # Do not eager load code on boot.
   config.eager_load = false
-
+  config.hosts << "diaryshare-backend.herokuapp.com"
   # Show full error reports.
   config.consider_all_requests_local = true
 
