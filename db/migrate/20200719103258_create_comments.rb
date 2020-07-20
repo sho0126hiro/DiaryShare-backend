7898a249c3603af7898b1fae8c1fcac324497cd8
sho0126hiro <sho0126hiro@gmail.com>
2020-07-20 14:31:04 +0900

    migrate file bug fix

--- db/migrate/20200719103258_create_comments.rb
+++ db/migrate/20200719103258_create_comments.rb
@@ -2,7 +2,7 @@ class CreateComments < ActiveRecord::Migration[6.0]
   def change
     create_table :comments do |t|
       t.string :user_id, null: false
-      t.string :diary_id, null: false
+      t.integer :diary_id, null: false
       t.string :content, null: false
 
       t.timestamps
--- db/migrate/20200719103900_create_diary_reactions.rb
+++ db/migrate/20200719103900_create_diary_reactions.rb
@@ -2,10 +2,10 @@ class CreateDiaryReactions < ActiveRecord::Migration[6.0]
   def change
     create_table :diary_reactions do |t|
       t.string :user_id, null: false
-      t.string :diary_id, null: false
+      t.integer :diary_id, null: false
       t.string :reaction_type, null: false
       t.timestamps
     end
-    add_index :diary_reactions [:user_id, :diary_id], unique: true
+    add_index :diary_reactions, [:user_id, :diary_id], unique: true
   end
 end
